9bac00b5ee7b849cdbb30725ddb1df35ba2e7e14
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-05-01 12:31:36 +0530

    Implementing E-Commerce Backend System

---
+++ src/test/java/com/abhishek/ecommercebackendsystem/ECommerceBackendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.ecommercebackendsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
